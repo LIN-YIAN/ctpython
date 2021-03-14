3c983d0edc8c5955e91c25e48292f9d32948060c
LIN-YIAN <80541701+LIN-YIAN@users.noreply.github.com>
2021-03-14 16:27:51 +0800

    Add files via upload

---
+++ 031401.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print(range(10))
+print(list(range(10)))    #list列印出來
+print(list(range(1,10)))  #1為起始值(有包含),10為終止值(不包含)
+print(list(range(1,10,2)))
